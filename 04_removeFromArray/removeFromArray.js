6d3ed1222950a2b041b5afb4db6514a36a564460
Szymon Fratczak <42326374+sfratczak@users.noreply.github.com>
2022-11-28 14:28:26 +0100

    Complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array) {
+  for (let argsIndex = 1; argsIndex <= arguments.length; argsIndex++) {
+    for (let arrIndex = 0; arrIndex < array.length; arrIndex++) {
+      if (array[arrIndex] === arguments[argsIndex]) {
+        array.splice(arrIndex, 1);
+      }
+    }
+  }
 
+  return array;
 };
 
 // Do not edit below this line
