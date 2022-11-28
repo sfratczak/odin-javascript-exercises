b039f9e6df8dadc1e156b29f03ca5ece53c46bc7
Szymon Fratczak <42326374+sfratczak@users.noreply.github.com>
2022-11-28 15:11:57 +0100

    Complete exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  // divisible by 400 or (divisible by 4 but not 100)
+  if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
