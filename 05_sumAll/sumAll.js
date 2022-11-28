3b37acdcf9e935516f86ea16e915039278411874
Szymon Fratczak <42326374+sfratczak@users.noreply.github.com>
2022-11-28 14:53:46 +0100

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (typeof num1 != "number" || typeof num2 != "number") return "ERROR";
+  if (num1 < 0 || num2 < 0) return "ERROR";
 
+  const startNumber = num1 < num2 ? num1 : num2;
+  const endNumber = num1 < num2 ? num2 : num1;
+
+  let result = 0;
+  let currentNumber = startNumber;
+
+  while (currentNumber <= endNumber) {
+    result += currentNumber;
+    currentNumber++;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
