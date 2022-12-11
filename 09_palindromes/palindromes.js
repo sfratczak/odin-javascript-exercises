3a7e9a50647d87b9dcb170bffe2b7ab11cf9a250
Szymon Fratczak <42326374+sfratczak@users.noreply.github.com>
2022-12-11 21:52:20 +0100

    Complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const filteredArray = Array.from(str.toLowerCase()).filter((char) =>
+    /[a-zA-Z]/.test(char)
+  );
+  const filteredString = filteredArray.join("");
+  const reversedString = filteredArray.reverse().join("");
 
+  return filteredString === reversedString;
 };
 
 // Do not edit below this line
