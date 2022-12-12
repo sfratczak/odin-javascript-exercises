92737cceeb2c8e3813cb09ae37355fcab1ca76ff
Szymon Fratczak <42326374+sfratczak@users.noreply.github.com>
2022-12-12 20:08:29 +0100

    Complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (Number(num) < 1) return "OOPS";
 
+  const numbers = [1, 1];
+
+  for (let i = 2; i < Number(num); i++) {
+    numbers.push(numbers[i - 2] + numbers[i - 1]);
+  }
+
+  return numbers[Number(num) - 1];
 };
 
 // Do not edit below this line
